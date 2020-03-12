b7218f464d9c8e90374b962792e23fe81fe5c4e6
Dylan Collins <dcollins5590@students.mc3.edu>
2020-03-12 17:18:16 -0400

    broke the one big test into 5 different one

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,29 +2,40 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
-test('checking to make the form and inputs are working properly and the submit button it rendering correctly ',()=>{
-
-    const {getByLabelText, getByTestId , findByText, findByDisplayValue} = render(<ContactForm/>);
-
+test('checking the first name input and onchange event ',()=>{
+    const {getByLabelText,findByText} = render(<ContactForm/>);
     const firstName = getByLabelText(/first name/i)
-    const lastName = getByLabelText(/last name/i)
-    const email = getByLabelText(/email/i)
-    const message = getByLabelText(/message/i)
-
     fireEvent.change(firstName, {target: {name: 'firstName', value: 'dylan'}})
+    findByText(/dylan/i);  
+} )
+
+test('checking the last name input and onchange event ',()=>{
+    const {getByLabelText,findByText} = render(<ContactForm/>);
+    const lastName = getByLabelText(/last name/i)
     fireEvent.change(lastName, {target: {name: 'lastName', value: 'collins'}})
-    fireEvent.change(email, {target: {name: 'email', value: 'dylancollins@aol.com'}})
-    fireEvent.change(message, {target: {name: 'message', value: 'hello world'}})
-
-    const button = getByTestId(/submit/i);
-    fireEvent.submit(button)
-
-    
-    findByDisplayValue(/submit/i)
-
-    findByText(/dylan/i);
     findByText(/collins/i);
+} )
+
+test('checking the email input and onchange event ',()=>{
+
+    const {getByLabelText,findByText} = render(<ContactForm/>);
+    const email = getByLabelText(/email/i)
+    fireEvent.change(email, {target: {name: 'email', value: 'dylancollins@aol.com'}})
     findByText(/dylancollins@aol.com/i);
+} )
+
+test('checking the message input and onchange event ',()=>{
+
+    const {getByLabelText,findByText} = render(<ContactForm/>);
+    const message = getByLabelText(/message/i)
+    fireEvent.change(message, {target: {name: 'message', value: 'hello world'}})
     findByText(/hellow world/i);  
 } )
+
+test('testing the submit button', ()=>{
+    const {getByTestId ,findByDisplayValue} = render(<ContactForm/>);
+    const button = getByTestId(/submit/i);
+    fireEvent.submit(button)
+    findByDisplayValue(/submit/i)
+})
 
