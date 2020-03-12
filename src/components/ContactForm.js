a489e1cbe7212012032db2516475131c93bcf671
Dylan Collins <dcollins5590@students.mc3.edu>
2020-03-12 16:21:25 -0400

    finished my MVP

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,37 +2,26 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
-  const [data, setData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    message: ''
-  });
+  const [data, setData] = useState();
+
 
   const { register, errors, handleSubmit, reset } = useForm({
     mode: "onBlur"
   });
-  const handleChange= e => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value
-    })
-  }
+
   const onSubmit = data => {
     setData(data);
   };
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form data-testid='submit' onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
             id="firstName"
             placeholder="bill"
-            onChange={handleChange}
-            value={data.firstName}
             ref={register({ required: true, maxLength: 15 })}
             />
           {errors.firstName && (
@@ -41,13 +30,11 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="lastName">Last Name*</label>
+          <label htmlFor="lastName" > Last Name*</label>
           <input
             name="lastName"
             id="lastName"
             placeholder="luo"
-            onChange={handleChange}
-            value={data.lastName}
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -59,14 +46,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input onChange={handleChange} value={data.email} name="email" id="email" ref={register({ required: true })} />
+          <input  name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea onChange={handleChange} value={data.message} name="message"  id="message"ref={register({ required: false })} />
+          <textarea  name="message"  id="message"ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,11 +3,28 @@ import {render, fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('checking to make the form and inputs are working properly and the submit button it rendering correctly ',()=>{
-    const {getByLabelText} = render(<ContactForm/>);
+
+    const {getByLabelText, getByTestId , findByText, findByDisplayValue} = render(<ContactForm/>);
 
     const firstName = getByLabelText(/first name/i)
     const lastName = getByLabelText(/last name/i)
     const email = getByLabelText(/email/i)
     const message = getByLabelText(/message/i)
 
+    fireEvent.change(firstName, {target: {name: 'firstName', value: 'dylan'}})
+    fireEvent.change(lastName, {target: {name: 'lastName', value: 'collins'}})
+    fireEvent.change(email, {target: {name: 'email', value: 'dylancollins@aol.com'}})
+    fireEvent.change(message, {target: {name: 'message', value: 'hello world'}})
+
+    const button = getByTestId(/submit/i);
+    fireEvent.submit(button)
+
+    
+    findByDisplayValue(/submit/i)
+
+    findByText(/dylan/i);
+    findByText(/collins/i);
+    findByText(/dylancollins@aol.com/i);
+    findByText(/hellow world/i);  
 } )
+
